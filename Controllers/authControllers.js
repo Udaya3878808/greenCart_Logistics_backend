59484a81faea9a0092a3ff35822bbc06998f2eba
udaya3878808 <udaya3878808@gmail.com>
2025-08-14 15:05:11 +0530

    Add root redirect and fix routing

--- Controllers/authControllers.js
+++ Controllers/authControllers.js
@@ -44,8 +44,9 @@ export const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      maxAge: 24 * 60 * 60 * 1000,
+      secure: process.env.NODE_ENV === "production", 
+      sameSite: "none", 
+      maxAge: 24 * 60 * 60 * 1000, // 1 day
     });
 
     res.json({ message: "Login successful" });
